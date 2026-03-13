17a98acb439065830767130c783db7fcc553d481
Manish kumar <memanish2525@gmail.com>
2026-03-13 19:55:48 +0530

    Day-2 React

---
+++ basic-Day-1/script.js
@@ -0,0 +1,73 @@
+// let arr=[1,2,3]
+// let arr2=arr
+
+
+
+// arr2.push(4)
+// console.log(arr2)
+
+
+// let arr=[1,2,3,4];
+// let arr2=[arr[0],arr[1],arr[2],arr[3]]
+// console.log(arr2)
+
+// let arr=[1,2,3,4];
+// let arr2=[...arr]
+// console.log(arr2)
+
+
+// let arr=[1,2,3,4]
+// let [a,b,c]=arr;
+// console.log(b)
+
+// let arr=['sonu','sunil','monu']
+// let [...arr2]=arr
+// console.log(arr2)
+
+// let arr2=[...arr]
+// arr2.push('sumit')
+// console.log(arr2)
+
+
+
+// let obj={
+//     user:'manish',
+//     age:12,
+//     city:'siwan'
+
+
+// }
+// let obj2={...obj}
+// obj2.name="sunil"
+// console.log(obj2)
+
+// let {user,age}=obj
+// console.log(user)
+
+
+// let obj={
+//     user:'manish',
+//     age:23,
+//     city:'Rachi',
+//     skills:['js','node','css']
+// }
+// let {skills}=obj
+
+// let [first]=skills
+
+// console.log(first)
+
+
+import user from "./app.js"
+import skills from "./test.js"
+import { obj } from "./test.js"
+import objl from "./test.js"
+
+
+
+console.log(user)
+console.log(skills)
+console.log(obj.name)
+console.log(objl)
+
+
---
+++ basic-Day-1/test.js
@@ -0,0 +1,19 @@
+
+
+let skills=['js','node','css']
+
+
+
+export const obj={
+    name:'hola',
+    age:22,
+    
+}
+
+const objl={
+    name:'golu dada',
+    age:30,
+    city:"londaon"
+}
+
+export default {skills,objl}
